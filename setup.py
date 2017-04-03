4029c0a388d4ecb84b5cc190275b8d1c7bf09762
Jan Funke <jfunke@iri.upc.edu>
2017-04-03 17:43:58 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pymatch',
+    version='0.0.1',
+    author='Jan Funke',
+    author_email='jfunke@iri.upc.edu',
+    description='A simple python module to compute the Hungarian matching using an LP.',
+    packages=['pymatch'],
+)
